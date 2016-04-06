b1fb19d515e8f32a84fa483372b4ebc7a524f950
Sarah <sarahkane682@yahoo.com>
2016-04-06 15:54:32 -0700

    done with baseline

---
+++ scrabble.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+
+
+class Scrabble < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  get '/score' do
+    erb :score
+  end
+
+  get '/score_many' do
+    erb :score_many
+  end
+
+  run!
+end
